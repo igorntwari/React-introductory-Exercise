bb6989ba6fee3538b89c9d2570ba2d3c33ebabc3
igorntwari <igorntwari28@gmail.com>
2023-10-02 11:40:55 +0200

    solved exercise03

--- hello-word/src/components/Button.js
+++ hello-word/src/components/Button.js
@@ -1,9 +1,15 @@
+import React from 'react';
+
 export default function Button() {
- 
- return (
+  const handleButtonClick = (buttonId) => {
+    alert(`Button ${buttonId} was clicked`);
+  };
+
+  return (
     <div>
-         <button onClick = {()=> {alert('cliked')}}>click Me</button>
+      <button onClick={() => handleButtonClick(1)}>click Me 1</button>
+      <button onClick={() => handleButtonClick(2)}>click Me 2</button>
+      <button onClick={() => handleButtonClick(3)}>click Me 3</button>
     </div>
-    
- )
+  );
 }
