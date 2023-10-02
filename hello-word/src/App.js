8105cc6c4bc2f957b1e89cf706461d0385bf41d1
igorntwari <igorntwari28@gmail.com>
2023-10-02 12:28:04 +0200

    solved exercise07

--- hello-word/src/App.js
+++ hello-word/src/App.js
@@ -1,10 +1,9 @@
+import Form from "./components/Form";
 
 export default function App() {
-  const itemList = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'].map(item => <li>{item}</li>)
-
   return (
     <div>
-      <ul>{itemList} </ul>
+    <Form />
     </div>
   );
 }
